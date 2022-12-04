f505f947243df0befbc62374ef158386ec5b0fae
Danica Gazdić <danicagazdic@gmail.com>
2022-12-04 21:37:05 +0100

    Added some controllers and services

---
+++ AirRide/src/main/java/com/uber/app/team23/AirRide/controller/DriverController.java
@@ -0,0 +1,13 @@
+package com.uber.app.team23.AirRide.controller;
+
+import com.uber.app.team23.AirRide.service.DriverService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController @RequestMapping("/vozac")
+public class DriverController {
+
+    @Autowired
+    private DriverService driverService;
+}
---
+++ AirRide/src/main/java/com/uber/app/team23/AirRide/service/DriverService.java
@@ -0,0 +1,9 @@
+package com.uber.app.team23.AirRide.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DriverService {
+
+
+}
