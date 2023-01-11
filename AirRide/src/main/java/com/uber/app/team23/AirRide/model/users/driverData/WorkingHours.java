ba877275b9d57c23831843d41efea8d2795f1fae
unknown <isomidobradovic@gmail.com>
2023-01-12 00:09:29 +0100

    Fixed WorkHours EP

--- AirRide/src/main/java/com/uber/app/team23/AirRide/model/users/driverData/WorkingHours.java
+++ AirRide/src/main/java/com/uber/app/team23/AirRide/model/users/driverData/WorkingHours.java
@@ -2,23 +2,24 @@ package com.uber.app.team23.AirRide.model.users.driverData;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Setter @Getter @NoArgsConstructor @AllArgsConstructor
+@Data
+@NoArgsConstructor @AllArgsConstructor
 @Entity @Table(name = "working_hours")
 public class WorkingHours {
     @Column(name = "start_time") @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime start;
+
     @Column(name = "end_time") @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime end;
+
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "driver_id")
     private Driver driver;
+
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
